06e1b829d0e1c2b1aefc7f47ea4557e4f85f7240
Sanjana Kolisetty <95172001+SanKolisetty@users.noreply.github.com>
2023-07-04 00:35:23 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow import keras
+import cv2 as cv
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from PIL import Image, ImageOps
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+img_size = 48
+
+model = tf.keras.models.load_model("AIGeneratedModel.h5")
+
+st.title("AI Image Classifier")       
+        
+img = st.file_uploader("Upload your Image")
+
+if img and st.button("Check"):
+    image = Image.open(img)
+    st.image(img)
+    image = ImageOps.fit(image, (48,48), Image.ANTIALIAS)
+    img_array = img_to_array(image)
+    new_arr = img_array/255
+    test = []
+    test.append(new_arr)
+    test = np.array(test).reshape(-1, img_size, img_size, 3)
+    y = model.predict(test)
+    if y[0] <= 0.5:
+        st.write("The given image is Real.")
+    else:
+        st.write("The given image is AI Generated.")
+    
